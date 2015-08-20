12027adf36607005583acd33a50f92400988c8c5
Harsh Joshi <me.harsh.joshi@gmail.com>
2015-08-20 20:12:12 +0530

    #PRAAT Directory info updated

--- initialize.py
+++ initialize.py
@@ -6,6 +6,8 @@ import os
 import sys
 if "C:\\PRAAT" not in sys.path:
     sys.path.append("C:\\PRAAT")
+#Assuming PRAAT in C:\PRAAT
+#Get PRAAT from : http://www.praat.org/
 
 from christPyLib import generalUtility
 from christPyLib import dspUtil
